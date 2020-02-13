320e337ed0d46951c6c3b3845080476dafd31a3c
maschon0 <michael.schon@gmi.oeaw.ac.at>
2020-02-13 18:59:34 +0100

    Refactored scripts to be callable as subcommands from main bookend program

---
+++ bookend/bookend.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+    
+    /|| bookend ||\\
+    ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+    End-guided transcript assembly
+    for short and long read RNA-seq
+"""
+
+import argparse
+import logging, logging.config
+import os
+import sys
+from bookend.core.argument_parsers import main_parser
+from bookend.__init__ import __version__,__updated__,__date__
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname)s %(name)s %(message)s'
+        },
+    },
+    'handlers': {
+        'stdout':{
+            'class' : 'logging.StreamHandler',
+            'stream'  : 'ext://sys.stdout',
+            'formatter': 'default',
+        },
+        'stderr':{
+            'class' : 'logging.StreamHandler',
+            'stream'  : 'ext://sys.stderr',
+            'level':'ERROR',
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'handlers': ['stdout','stderr'],
+        'level': 'INFO',
+    },
+}
+
+logging.config.dictConfig(LOGGING)
+log = logging.getLogger()
+
+def get_parser(program_version_message):
+    parser = main_parser
+    parser.add_argument('-v', '--version', action='version', version=program_version_message)
+    parser.add_argument('-l', '--loglevel',dest='log_level', help='Set log level', default='INFO', choices=['DEBUG','INFO','WARNING','ERROR'])
+    return parser
+
+
+def main():
+    """Passes commandline options to subfunctions."""
+    version = 'v{}'.format(__version__)
+    date = str(__updated__)
+    program_version_message = '{} ({})'.format(version, date)
+    
+    parser = get_parser(program_version_message)
+    args = vars(parser.parse_args())
+    try:
+        log_level = args['log_level']
+        log.setLevel(log_level)
+        obj = args['object'](args)
+        return obj.run()
+    except KeyboardInterrupt:
+        return 0
+    except Exception as e:
+        log.exception(e)
+        return 2
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ bookend/core/argument_parsers.py
@@ -0,0 +1,300 @@
+import argparse
+from bookend.core.assemble import Assembler
+from bookend.core.fastq_end_label import EndLabeler
+from bookend.core.bam_to_elr import BAMtoELRconverter
+# from core.bed_to_elr import BEDtoELRconverter
+# from core.elr_to_bed import ELRtoBEDconverter
+# from core.elr_combine import ELRcombiner
+# from core.fasta_index import FASTAindexer
+# from core.fasta_softbridges import FASTAsoftbridger
+# from core.sam_sj_out import SAMtoSJconverter
+# from core.sj_merge import SJmerger
+# from core.sj_to_bed import SJtoBEDconverter
+# from bookend.core.gtf_merge import AnnotationMerger
+
+#TODO: Flesh out Helper class
+class Helper:
+    def __init__(self, args):
+        self.no_args_message = """
+/| bookend |\\
+¯¯¯¯¯¯¯¯¯¯¯¯¯
+End-guided transcriptome assembly.
+usage: bookend [subcommand] [options] [input file(s)]
+Subcommands (use -h/--help for more info):
+
+    label    (Label 5' and 3' ends in a FASTQ file)
+    assemble (Assemble transcripts from aligned end-labeled reads)
+    merge    (Merge assembled GTFs with a reference annotation)
+
+    --end-labeled read (ELR) operations--
+    make-elr
+    sort-elr
+    collapse-elr
+    combine-elr
+
+    --file conversion--
+    bed-to-elr
+    elr-to-bed
+    gff3-to-bed
+    gtf-to-bed
+    sam-to-sj
+    sj-to-bed
+    merge-sj
+
+    --setup/indexing--
+    index-fasta
+    softbridge-fasta
+
+"""
+    
+    def run(self):
+        print(self.no_args_message)
+
+
+desc = 'Functions for end-guided assembly of RNA-seq data.'
+main_parser = argparse.ArgumentParser(description=desc)
+main_parser.set_defaults(object=Helper)
+subparsers = main_parser.add_subparsers(title='subcommands',description='Choose a command to run',help='Supported subcommands:')
+
+### assemble.py ###
+assemble_parser = subparsers.add_parser('assemble',help="Assembles an end-labeled read (ELR) file. Produces an output assembly (BED12/ELR/GTF) and a table of summary statistics (bookend_stats.tsv)")
+assemble_parser.add_argument('-o','--output', dest='OUT', type=str, default='bookend_assembly.gtf', help="Destination file for assembly. File extension (bed, elr, gtf) determines output type.")
+assemble_parser.add_argument('--source', dest='SOURCE', type=str, default='bookend', help="String to write in the source column of output GTF/ELR")
+assemble_parser.add_argument('--genome', dest='GENOME', type=str, default=None, help="Genome FASTA file. Used for end label filtering only if input is BAM/SAM.")
+assemble_parser.add_argument('--max_gap', dest='MAX_GAP', type=int, default=50, help="Largest gap size to tolerate (number of nucleotides).")
+assemble_parser.add_argument('--min_overhang', dest='MIN_OVERHANG', type=int, default=3, help="Smallest overhang to count for a read overlapping two exon fragments (number of nucleotides).")
+assemble_parser.add_argument('--allow_incomplete', dest='INCOMPLETE', default=False, action='store_true', help="Filter out all assembled transcripts that are not end-to-end complete.")
+assemble_parser.add_argument('--infer_starts', dest='INFER_STARTS', default=False, action='store_true', help="If S tags are missing, calculate the most likely start site based on coverage changes.")
+assemble_parser.add_argument('--infer_ends', dest='INFER_ENDS', default=False, action='store_true', help="If E tags are missing, calculate the most likely end site based on coverage changes.")
+assemble_parser.add_argument('--end_cluster', dest='END_CLUSTER', type=int, default=100, help="Largest distance between ends to consider in one cluster (number of nucleotides).")
+assemble_parser.add_argument('--min_cov', dest='MIN_COV', type=float, default=1.5, help="Minimum coverage filter to remove low-evidence transcript models.")
+assemble_parser.add_argument('--min_unstranded_cov', dest='MIN_UNSTRANDED', type=float, default=20, help="(Only used if --allow_incomplete) Set a more stringent threshold for keeping nonstranded frags.")
+assemble_parser.add_argument('--min_start', dest='MIN_S', type=float, default=1, help="Minimum number of start reads to accept as a start site.")
+assemble_parser.add_argument('--min_end', dest='MIN_E', type=float, default=1, help="Minimum number of end reads to accept as an end site")
+assemble_parser.add_argument('--minlen', dest='MINLEN', type=int, default=50, help="Minimum output transcript length (nucleotides).")
+assemble_parser.add_argument('--min_proportion', dest='MIN_PROPORTION', type=float, default=0.01, help="[float 0-1] Exclude ends, juctions, or transcripts that contribute < this proportion. (Used as a signal threshold)")
+assemble_parser.add_argument('--intron_filter', dest='INTRON_FILTER', type=float, default=0.15, help="[float 0-1] Retained introns must exceed this proportion the be considered.")
+assemble_parser.add_argument('--cap_percent', dest='CAP_PERCENT', type=float, default=0.05, help="[float 0-1] Exclude 5' end features with < this percent of cap-labeled reads (reads that contain upstream untemplated G).")
+assemble_parser.add_argument('--verbose', dest='VERBOSE', default=False, action='store_true', help="Display a verbose summary of each assembly in stdout.")
+assemble_parser.add_argument(dest='INPUT', type=str, help="Input BED/ELR filepath. MUST be a single coordinate-sorted file.")
+assemble_parser.set_defaults(object=Assembler)
+
+### bam_to_elr.py ###
+bam_to_elr_parser = subparsers.add_parser('make-elr',help="Converts a BAM or SAM file to an End-Labeled Read (ELR) or BED12 file.")
+bam_to_elr_parser.add_argument("-o", "--output", dest='OUTPUT', type=str, default=None, help="Filepath to write end-labeled file.")
+bam_to_elr_parser.add_argument("--source", dest='SOURCE', default=None, type=str, help="Name the source of BAM/SAM reads.")
+bam_to_elr_parser.add_argument("--genome", dest='GENOME', default=None, type=str, help="Genome FASTA file")
+bam_to_elr_parser.add_argument("--stranded", dest='STRANDED', default=False, action="store_true", help="The reads are strand-specific")
+bam_to_elr_parser.add_argument("-s", dest='START', default=False, action='store_true', help="Read 5' ends are transcript start sites.")
+bam_to_elr_parser.add_argument("-c", dest='CAPPED', default=False, action='store_true', help="5' end data is capped.")
+bam_to_elr_parser.add_argument("-e", dest='END', default=False, action='store_true', help="Read 3' ends are transcript end sites.")
+bam_to_elr_parser.add_argument("--no_ends", dest='NO_ENDS', default=False, action='store_true', help="(overrides other options) Ignore all 5' and 3' end labels.")
+bam_to_elr_parser.add_argument("--bed", dest='BED_OUT', default=False, action='store_true', help="Output a 15-column end labeled BED file.")
+bam_to_elr_parser.add_argument("--secondary", dest='SECONDARY', default=False, action='store_true', help="Keep secondary alignments.")
+bam_to_elr_parser.add_argument("--header", dest='HEADER', type=str, default=None, help="Filepath to write ELR header.")
+bam_to_elr_parser.add_argument("--start_seq", dest='START_SEQ', default='ACGGG', type=str, help="Sequence of the oligo that marks a 5' read (sense)")
+bam_to_elr_parser.add_argument("--end_seq", dest='END_SEQ', default='RRRRRRRRRRRRRRR', type=str, help="Sequence of the oligo that marks a 3' read (sense)")
+bam_to_elr_parser.add_argument("--record_artifacts", dest='RECORD_ARTIFACTS', default=False, action='store_true', help="Reports artifact-masked S/E labels as lowercase s/e.")
+bam_to_elr_parser.add_argument("--split", dest='SPLIT', default=False, action='store_true', help="Separate reads into different files by their multimapping number.")
+bam_to_elr_parser.add_argument("--mismatch_rate", dest='MM_RATE', default=0.20, type=float, help="Mismatch tolerance of S label matches")
+bam_to_elr_parser.add_argument("--sj_shift", dest='SJ_SHIFT', default=2, type=int, help="Shift up to this many bases to find a canonical splice junction")
+bam_to_elr_parser.add_argument("--minlen", dest='MINLEN', default=20, type=int, help="Minimum number of nucleotides for an aligned read.")
+bam_to_elr_parser.add_argument("INPUT", type=str, default=None, help="Input BAM/SAM file")
+bam_to_elr_parser.set_defaults(object=BAMtoELRconverter)
+
+### bed_to_elr.py ###
+# desc = (
+#     "Converts BED files to a 'end labeled read' (ELR) format.\n"
+#     "ELR contains a two-component header: #G (genome) and #N (names of samples).\n"
+#     "Each line is a read or read stack with six columns:\n"
+#     "  chromosome  position  strand  ELCIGAR  sample  weight\n"
+#     "\n"
+#     "  chromosome: Chromosome number, as indexed by the #G header\n"
+#     "  position: 0-indexed position in chromosome\n"
+#     "  strand: Inferred RNA strand; +, -, or . (unknown)\n"
+#     "  ELCIGAR: String describing the mapped read (full description below)\n"
+#     "  sample: Sample number, as indexed by the #N header\n"
+#     "  weight: Read count (allows partial counts for multimappers)\n"
+# )
+
+# epilog = (
+#     "ELCIGAR strings are Character|Number strings and one trailing character\n"
+#     "([CN]xC), where C is a label and N is a numeric length on the genome.\n"
+#     "Each C labels an end or gap in the alignment as one of the following:\n"
+#     "  S: start\n"
+#     "  s: start (low confidence)\n"
+#     "  C: start (capped)\n"
+#     "  E: end (polyA tail)\n"
+#     "  e: end (low confidence)\n"
+#     "  D: splice junction donor\n"
+#     "  A: splice junction acceptor\n"
+#     "  .: unspecified gap or end\n"
+#     "\n"
+#     "For example, a paired-end 50 read of a 185-nt fragment would be\n"
+#     "  x50x85x50x\n"
+#     "A full 3-exon transcript could be described as:\n"
+#     "  S256D800A128D800A512E\n"
+#     "where the 3 exons are 256, 128, and 512 nucleotides,\n"
+#     "and the 2 introns are both 800 nucleotides.\n"
+#     "\n"
+# )
+
+# # initilize argumentparser to read in commands
+# parser = argparse.ArgumentParser(
+#     description=desc,
+#     epilog=epilog,
+#     formatter_class=RawTextHelpFormatter
+# )
+# parser.add_argument(
+#     "--source", dest='SOURCE',
+#     help="Source of BED lines.",
+#     default=None, type=str
+# )
+# parser.add_argument(
+#     "-j", dest='JUNCTIONS', default=False, action='store_true',
+#     help="Gaps in the reads are splice junctions."
+# )
+# parser.add_argument(
+#     "-s", dest='START', default=False, action='store_true',
+#     help="Read 5' ends are transcript start sites."
+# )
+# parser.add_argument(
+#     "-c", dest='CAPPED', default=False, action='store_true',
+#     help="5' end data is capped."
+# )
+# parser.add_argument(
+#     "-e", dest='END', default=False, action='store_true',
+#     help="Read 3' ends are transcript end sites."
+# )
+# parser.add_argument(
+#     "--bed", dest='BED_OUT', default=False, action='store_true',
+#     help="Output a 15-column end labeled BED file."
+# )
+# parser.add_argument(
+#     "-O", "--output", dest='OUTPUT', type=str, default='stdout',
+#     help="Filepath to write ELR file."
+# )
+# parser.add_argument(
+#     "--header", dest='HEADER', type=str, default='stdout',
+#     help="Filepath to write ELR header."
+# )
+# parser.add_argument(
+#     "FILENAME", nargs='?'
+# )
+
+### elr_combine.py ###
+# parser = argparse.ArgumentParser()
+# parser.add_argument(
+#     dest='INPUT',
+#     help="Input sorted ELR file(s).",
+#     type=str,
+#     nargs='+'
+# )
+
+# args = parser.parse_args()
+
+### elr_to_bed.py ###
+# desc = (
+#     "Converts 'end labeled read' (ELR) files to BED.\n"
+#     "Three additional columns are added to the traditional 12-column format:\n"
+#     "\n"
+#     "  weight: Read count (allows partial counts for multimappers)\n"
+#     "  sample: Source of the ELR entry\n"
+#     "  end label: String describing the ends of each block as a start, end, junction, or none\n"
+# )
+# # initilize argumentparser to read in commands
+# parser = argparse.ArgumentParser(
+#     description=desc,
+#     formatter_class=RawTextHelpFormatter
+# )
+# parser.add_argument(
+#     "-O", "--output", dest='OUTPUT', type=str, default='stdout',
+#     help="Filepath to write ELR file."
+# )
+# parser.add_argument(
+#     "FILENAME", nargs='?'
+# )
+
+### fastq_end_label.py ###    
+end_label_parser = subparsers.add_parser('label',help="Trims and labels RNA 5' and 3' ends in a FASTQ file")
+end_label_parser.add_argument('-S', '--start', dest='START', type=str, default='AAGCAGTGGTATCAACGCAGAGTACGGG', help="Template switching primer, marks the RNA 5' terminus.")
+end_label_parser.add_argument( '-E', '--end', dest='END', type=str, default='AAGCAGTGGTATCAACGCAGAGTACTTTTTTTTTTTTTTTTTTTTTT+', help="Reverse transcription primer, marks (reverse complement of) the RNA 3' terminus.")
+end_label_parser.add_argument('--strand', dest='STRAND', type=str, default='unstranded', choices=['forward','reverse','unstranded'], help="Orientation of mate1 with respect to the RNA strand")
+end_label_parser.add_argument('--out1', dest='OUT1', type=str, default='end_label.1.fastq', help="Destination file for trimmed and labeled mate 1 FASTQ file.")
+end_label_parser.add_argument('--out2', dest='OUT2', type=str, default='end_label.2.fastq', help="Destination file for trimmed and labeled mate 2 FASTQ file.")
+end_label_parser.add_argument('--single_out', dest='SINGLE_OUT', type=str, default='end_label.single.fastq', help="Destination file for reads that are only informative single-end.")
+end_label_parser.add_argument('--min_start', dest='MIN_START', type=int, default=7, help="Minimum number of nucleotides that must match to the start adapter sequence.")
+end_label_parser.add_argument('--min_end', dest='MIN_END', type=int, default=9, help="Minimum number of nucleotides that must match to the end adapter sequence.")
+end_label_parser.add_argument('--suppress_untrimmed', dest='SUPPRESS_UNTRIMMED', default=False, action='store_true', help="If no trimming occurred, do not write the read to output.")
+end_label_parser.add_argument('--verbose', dest='VERBOSE', default=False, action='store_true', help="Display each trimming result on stdout.")
+end_label_parser.add_argument('--minlen', dest='MINLEN', type=int, default=16, help="Minimum sequence length to keep.")
+end_label_parser.add_argument('--mismatch_rate', dest='MM_RATE', type=float, default=.06, help="Highest allow proportion of mismatches.")
+end_label_parser.add_argument('--qualmask', dest='QUALMASK', type=float, default=16, help="Ignores any basecalls with phred score < this, treats base as N.")
+end_label_parser.add_argument('--minqual', dest='MINQUAL', type=float, default=25, help="Suppresses any trimmed sequences with lower than this mean phred quality score.")
+end_label_parser.add_argument(dest='FASTQ', type=str, nargs='+', help="Input FASTQ file(s). 1 for single-end, 2 for paired-end")
+end_label_parser.set_defaults(object=EndLabeler)
+
+### fasta_index.py ###
+# parser.add_argument(
+#     'FASTA', type=str,
+#     help="Path to FASTA file."
+# )
+# parser.add_argument(
+#     '-S','--split', dest='SPLIT_ON', type=str,
+#     help="Character string to split header name from description.",
+#     default=' '
+# )
+
+# ### fasta_softbridges.py ###
+# parser.add_argument(
+#     "-F", "--fasta", dest='FASTA',
+#     help="Genome FASTA file",
+#     default=None, type=str, required=True
+# )
+# parser.add_argument(
+#     "-O", "--output", dest='OUTPUT', type=str, default='stdout',
+#     help="Filepath to write BED12 file."
+# )
+# parser.add_argument(
+#     "--maxlen", dest='MAXLEN',
+#     help="Maximum tolerated length for a bridge over a softmasked region.",
+#     default=1000, type=int
+# )
+
+### gtf_merge.py ###
+# merge_parser = subparsers.add_parser('merge',help="Merges multiple assembly/annotation GTF/GFF3 files to one consensus")
+# merge_parser.add_argument('-r', dest='reference_GFF', help="[GFF3/GTF] Path to reference annotation(s)", nargs='*')
+# merge_parser.add_argument('-i', dest='input_GFF', help="[GFF3/GTF] Path to input annotation(s)", nargs='+')
+# merge_parser.add_argument('--genome', dest='genome_fasta', help="Path to a FASTA file of the genome.", default=None)
+# merge_parser.add_argument("--tracking", dest='tracking_file', help="gffcompare .tracking file.", default=None)
+# merge_parser.add_argument("--stats", dest='stats_file', help="bookend .stats file.", default=None)
+# merge_parser.add_argument("--buffer", dest='buffer', help="Number of allowed nonoverlapping terminal nucleotides.", default=100, type=int)
+# merge_parser.add_argument("--fasta_out", dest='fasta_out', help="Output FASTA file of transcript sequences.", default=None, type=str)
+# merge_parser.add_argument("--protein_out", dest='protein_out', help="Output FASTA file of amino acid sequences.", default=None, type=str)
+# merge_parser.set_defaults(object=AnnotationMerger)
+
+### sam_sj_out.py ###
+# parser.add_argument(
+#     "-F", "--fasta", dest='FASTA',
+#     help="Genome FASTA file",
+#     default=None, type=str, required=True
+# )
+# parser.add_argument(
+#     "--format", dest='FORMAT',
+#     help="Output file format",
+#     default='star', type=str, choices=['bed','star']
+# )
+# parser.add_argument(
+#     "--filter", dest='FILTER',
+#     help="Remove noncanonical splice junctions from the output",
+#     default=False, action='store_true'
+# )
+# parser.add_argument(
+#     "FILENAME", nargs='?'
+# )
+
+### sj_merge.py ###
+
+### sj_to_bed.py ###
+
+
