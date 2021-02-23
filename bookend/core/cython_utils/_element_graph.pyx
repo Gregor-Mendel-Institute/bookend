39992c73a1ad091ec5bd4f32502e0578071d4fcc
Schon <michael.schon@gmi.oeaw.ac.at>
2021-02-23 14:25:58 +0100

    calculate_extension_score() maximizes new bases

--- bookend/core/cython_utils/_element_graph.pyx
+++ bookend/core/cython_utils/_element_graph.pyx
@@ -282,18 +282,18 @@ cdef class ElementGraph:
             set new_members
             float bases, new_cov, similarity, e_bases
             np.ndarray e_prop, e_weights, proportions, path_proportions
-            new_members = set()
-            bases = path.bases
-            path_proportions = path.weights/path.cov
-            proportions = path_proportions*path.bases
-            for i in extension:
-                element = self.elements[i]
-                e_prop = self.available_proportion(path.weights, element)
-                e_weights = e_prop*element.weights
-                e_bases = np.sum(e_weights)*element.length
-                bases += e_bases
-                proportions += e_weights*element.length
-                new_members.update(element.members.difference(path.members))
+        new_members = set()
+        bases = path.bases
+        path_proportions = path.weights/path.cov
+        proportions = path_proportions*path.bases
+        for i in extension:
+            element = self.elements[i]
+            e_prop = self.available_proportion(path.weights, element)
+            e_weights = e_prop*element.weights
+            e_bases = np.sum(e_weights)*element.length
+            bases += e_bases
+            proportions += e_weights*element.length
+            new_members.update(element.members.difference(path.members))
         
         proportions /= bases
         new_length = sum([path.frag_len[i] for i in new_members])
