0986dd137951fabf85d188965e3a91f238058400
Schon <michael.schon@gmi.oeaw.ac.at>
2021-03-10 11:30:22 +0100

    Do not use add_gaps()

--- bookend/core/cython_utils/_assembly_utils.pyx
+++ bookend/core/cython_utils/_assembly_utils.pyx
@@ -196,7 +196,7 @@ cdef class Locus:
             for rng in self.end_ranges[endtype]:
                 prohibited_positions.update(range(rng.left, rng.right+1))
         
-        self.add_gaps(prohibited_positions)
+        # self.add_gaps(prohibited_positions)
         if 0 not in prohibited_positions:self.branchpoints.add(0)
         if len(self) not in prohibited_positions:self.branchpoints.add(len(self))
     
@@ -1606,7 +1606,7 @@ cpdef np.ndarray find_linear_chains(np.ndarray[char, ndim=2] overlap_matrix, np.
     np.put(containment, range(0,containment.shape[0]**2,containment.shape[0]+1), False, mode='wrap') # Blank out the diagonal (self-containments)
     contained = set(np.where(np.sum(containment,axis=1)>0)[0])
     in_chain = np.zeros(len(ingroup), dtype=np.int32)
-    putative_chain_starts = np.where(outgroup == 1)[0]
+    putative_chain_starts = np.where(np.logical_or(ingroup == 0, outgroup == 1))[0]
     chain = 0
     for v in putative_chain_starts:
         if in_chain[v] == 0: # v is unvisited
