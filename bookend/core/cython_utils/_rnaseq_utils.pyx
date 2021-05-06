55aab9a02326186b614fbbd0bff30bbded42fe8b
Schon <michael.schon@gmi.oeaw.ac.at>
2021-05-06 21:32:44 +0200

    import_annotation() updated to be compatible with GTF2.2

--- bookend/core/cython_utils/_rnaseq_utils.pyx
+++ bookend/core/cython_utils/_rnaseq_utils.pyx
@@ -1042,8 +1042,8 @@ cdef class AnnotationDataset(RNAseqDataset):
             ranges += [child.span]
 
         input_data = ELdata(chrom, source, strand, ranges, splice, True, True, False, 1)
-        parent.attributes['transcript_id'] = child.attributes.get('transcript_id', parent.attributes['transcript_id'])
         mapping_object = RNAseqMapping(input_data, parent.attributes)
+        mapping_object.attributes['transcript_id'] = child.transcript_id
         if 'cov' in mapping_object.attributes.keys():
             mapping_object.weight = float(mapping_object.attributes['cov'])
         
