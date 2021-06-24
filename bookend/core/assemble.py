fe4cb8832a4f0c74fda31af6a0c202c8930cbe98
Schon <michael.schon@gmi.oeaw.ac.at>
2021-06-25 01:25:06 +0200

    Implemented TSV output of estimated coverage by source

--- bookend/core/assemble.py
+++ bookend/core/assemble.py
@@ -142,7 +142,7 @@ class Assembler:
                             for k,v in locus.assembly_source_cov[transcript.attributes['transcript_id']].items():
                                 source_cov[k] = v
                             
-                            self.covfile.write('{}\t{}\n'.format(transcript.attributes['transcript_id'],'\t'.join([str(round(v,1)) for v in source_cov]))
+                            self.covfile.write('{}\t{}\n'.format(transcript.attributes['transcript_id'], '\t'.join([str(round(v,1)) for v in source_cov])))
                         
                         if self.verbose:
                             bases_used += transcript.attributes['bases']
